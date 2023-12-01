12e4be75f2c99ceef046cc5c7eaceb20c865778a
unknown <kevinnempon033@gmail.com>
2023-12-01 16:48:40 +0100

    fin

--- MenuPrincipal.js
+++ MenuPrincipal.js
@@ -1,50 +1,37 @@
+"use strict";
+
 const slides = document.querySelectorAll(".slide");
 
-// loop through slides and set each slides translateX property to index * 100% 
+
 slides.forEach((slide, indx) => {
   slide.style.transform = `translateX(${indx * 100}%)`;
 });
-// current slide counter
-let curSlide = 0;
 
-// select next slide button
+
 const nextSlide = document.querySelector(".btn-next");
 
-// add event listener and next slide functionality
-nextSlide.addEventListener("click", function () {
-     curSlide++;
-
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
-
-
+let curSlide = 0;
 let maxSlide = slides.length - 1;
 
 
 nextSlide.addEventListener("click", function () {
-  
+
   if (curSlide === maxSlide) {
     curSlide = 0;
   } else {
     curSlide++;
   }
-
-
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
-
 const prevSlide = document.querySelector(".btn-prev");
 prevSlide.addEventListener("click", function () {
-
   if (curSlide === 0) {
     curSlide = maxSlide;
-  } else {curSlide-- ;
+  } else {
+    curSlide--;
   }
-
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
