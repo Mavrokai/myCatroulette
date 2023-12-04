815a4c61851c04cf87d5b8180feb2aff55ead7bc
unknown <romainvasseur59940@gmail.com>
2023-12-04 09:11:38 +0100

    update

---
+++ Catnapper.js
@@ -0,0 +1,12 @@
+var audio = new Audio("./jeu/son/Nounours.mp3");
+var audio2 = new Audio("./jeu/son/Nounours.mp3");
+
+function func() {
+  console.log(audio);
+  audio.play();
+}
+
+function func2() {
+    console.log(audio);
+    audio2.play();
+  }
