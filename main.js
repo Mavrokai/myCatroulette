b948bc7ef4a4543676bc8b552f3631d99b19cd6c
unknown <valentin.59370@outlook.fr>
2023-12-05 14:02:37 +0100

    feur

---
+++ main.js
@@ -0,0 +1,5 @@
+const BR = document.getElementById('BR');
+
+BR.addEventListener('click', function() {
+  window.history.back(); // Cela renvoie l'utilisateur à la page précédente dans l'historique du navigateur
+});
