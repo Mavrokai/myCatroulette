3f8eef64014047d992e78acfd185f113c48bfa25
unknown <romainvasseur59940@gmail.com>
2023-12-06 11:20:03 +0100

    MaJ liens profils

--- ChoixProfils.js
+++ ChoixProfils.js
@@ -1,42 +1,48 @@
-
 "use strict";
 
 const slides = document.querySelectorAll(".slide");
 const nextSlide = document.querySelector(".btn-next");
 const prevSlide = document.querySelector(".btn-prev");
-
+const monBouton = document.getElementById('Pelote');
 
 let curSlide = 0;
 let maxSlide = slides.length - 1;
 
 function updateContentVisibility() {
-  // Mettez en œuvre la logique pour afficher ou masquer les contenus en fonction de curSlide
-  // Utilisez les identifiants uniques des éléments de contenu associés à chaque image
   document.querySelectorAll(".content").forEach(content => {
     content.style.display = "none";
   });
 
-  // Ajoutez des conditions pour chaque diapositive
-  if (curSlide === 0) {
-    document.querySelector(".contentForSlide0").style.display = "block";
-    document.querySelector(".contentForSlide1").style.display = "none";
-    document.querySelector(".contentForSlide2").style.display = "none";
-    document.querySelector(".contentForSlide3").style.display = "none";
-  } else if (curSlide === 1) {
-    document.querySelector(".contentForSlide0").style.display = "none";
-    document.querySelector(".contentForSlide1").style.display = "block";
-    document.querySelector(".contentForSlide2").style.display = "none";
-    document.querySelector(".contentForSlide3").style.display = "none";
-  } else if (curSlide === 2) {
-    document.querySelector(".contentForSlide0").style.display = "none";
-    document.querySelector(".contentForSlide1").style.display = "none";
-    document.querySelector(".contentForSlide2").style.display = "block";
-    document.querySelector(".contentForSlide3").style.display = "none";
-  } else if (curSlide === 3) {
-    document.querySelector(".contentForSlide0").style.display = "none";
-    document.querySelector(".contentForSlide1").style.display = "none";
-    document.querySelector(".contentForSlide2").style.display = "none";
-    document.querySelector(".contentForSlide3").style.display = "block";
+  slides.forEach((slide, indx) => {
+    const contentForSlide = document.querySelector(`.contentForSlide${indx}`);
+    if (indx === curSlide) {
+      slide.style.transform = `translateX(0)`;
+      contentForSlide.style.display = "block";
+      updateButtonLink(indx);
+    } else {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+      contentForSlide.style.display = "none";
+    }
+  });
+}
+
+function updateButtonLink(position) {
+  switch (position) {
+    case 0:
+      monBouton.setAttribute('onclick', 'window.location.href="profil.html";');
+      break;
+    case 1:
+      monBouton.setAttribute('onclick', 'window.location.href="profil1.html";');
+      break;
+    case 2:
+      monBouton.setAttribute('onclick', 'window.location.href="profil2.html";');
+      break;
+    case 3:
+      monBouton.setAttribute('onclick', 'window.location.href="profil3.html";');
+      break;
+    default:
+      // Gérer d'autres cas si nécessaire
+      break;
   }
 }
 
@@ -53,10 +59,6 @@ nextSlide.addEventListener("click", function () {
     curSlide++;
   }
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-
   // Mettez à jour la visibilité des contenus en fonction de la diapositive actuelle
   updateContentVisibility();
 });
@@ -69,14 +71,12 @@ prevSlide.addEventListener("click", function () {
     curSlide--;
   }
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-
   // Mettez à jour la visibilité des contenus en fonction de la diapositive actuelle
   updateContentVisibility();
 });
 
+// Mettez à jour la visibilité des contenus au chargement initial
+updateContentVisibility();
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
