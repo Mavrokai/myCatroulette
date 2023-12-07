e96b8c57662f2b80f51606d6bde0cd9c0a7bdc8e
unknown <kevinnempon033@gmail.com>
2023-12-07 13:26:12 +0100

    Patch

---
+++ profil personnel.js
@@ -0,0 +1,21 @@
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+  }
+
+  function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+    document.getElementById("photorect").style.display = "none";
+    document.getElementById("open-btn").style.display = "none";
+  }
+
+  function accepte() {
+    document.getElementById("popupForm").style.display = "none";
+    document.getElementById("photorect").style.display = "none";
+    document.getElementById("open-btn").style.display = "none";
+  }
+  
+  function appa() {
+      document.getElementById("friend").style.display = "block";
+      document.getElementById("s3").style.display = "block";
+      document.getElementById("chat3").style.display = "block";
+    }
